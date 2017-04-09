469e3ddeb8206f0cc2e77bc5c20e12e6b20c5078
NicolasLYU <673092847@qq.com>
2017-04-09 20:23:45 +0200

    Update sketch_short_example.ino

--- sketch_short_example/sketch_short_example.ino
+++ sketch_short_example/sketch_short_example.ino
@@ -4,8 +4,12 @@
 // Public Domain
 #include<Wire.h>
 const int MPU_addr=0x68;  // I2C address of the MPU-6050
+const g = 9.8;
 int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
 float Ax,Ay,Az,Gx,Gy,Gz;
+float Vx,Vy,Vz;
+float angleRoll,anglePitch;
+unsigned long time_current, time_last;
 void setup(){
   Wire.begin();
   Wire.beginTransmission(MPU_addr);
@@ -13,6 +17,20 @@ void setup(){
   Wire.write(0);     // set to zero (wakes up the MPU-6050)
   Wire.endTransmission(true);
   Serial.begin(9600);
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  angleRoll = acos(sqrt(AcX*AcX+AcZ*AcZ)/g);
+  anglePitch = acos(sqrt(AcY*AcY+AcZ*AcZ)/g);
+  Vx=0;
+  Vy=0;
+  Vz=0;
+  time_current = 0;
+  time_last = 0;
 }
 void loop(){
   Wire.beginTransmission(MPU_addr);
@@ -37,17 +55,23 @@ void loop(){
   Ax=2*9.8*(AcX-700)/32768;   //700
   Ay=2*9.8*(AcY-400)/32768;    //400
   Az=2*9.8*(AcZ+2400)/32768;    //-2400
+  time_current = millis();
+  Vx=Vx + Ax*0.001*(time_current - time_last);
+  Vy=Vy + Ay*0.001*(time_current - time_last);
+  Vz=Vz + Az*0.001*(time_current - time_last);
+  time_last = time_current;
   Gx=250*GyX/32768;
   Gy=250*GyY/32768;
   Gz=250*GyZ/32768;
-  Serial.print("Ax = "); Serial.print(Ax);
-  Serial.print(" | Ay = "); Serial.print(Ay);
-  Serial.print(" | Az = "); Serial.print(Az);
-  Serial.print(" | Gx = "); Serial.print(Gx);
-  Serial.print(" | Gy = "); Serial.print(Gy);
-  Serial.print(" | Gz = "); Serial.print(Gz);
+  Serial.print("Ax = "); Serial.print(Ax); Serial.print("m/s-2");
+  Serial.print(" | Ay = "); Serial.print(Ay); Serial.print("m/s-2");
+  Serial.print(" | Az = "); Serial.print(Az); Serial.print("m/s-2");
+  Serial.print(" | Gx = "); Serial.print(Gx); Serial.print("rad/s");
+  Serial.print(" | Gy = "); Serial.print(Gy); Serial.print("rad/s");
+  Serial.print(" | Gz = "); Serial.print(Gz); Serial.print("rad/s");
+  Serial.print(" |Vx = "); Serial.print(Vx); Serial.print("m/s");
+  Serial.print(" | Vy = "); Serial.print(Vy); Serial.print("m/s");
+  Serial.print(" | Vz = "); Serial.print(Vz); Serial.print("m/s");
   Serial.println("---------------------");
-  
-  delay(1000);
+  delay(333);
 }
-
